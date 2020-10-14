3b3365bbd11a1c1c63fe0ee7a1e30d660af80ece
Egor Zheludkov <zheludkov.egor@mail.ru>
2020-10-14 20:04:45 +0300

    05 - Добавили локализацию в Blazor

--- src/Frontend/Web/Chilite.Web/Program.cs
+++ src/Frontend/Web/Chilite.Web/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Chilite.Protos;
 using Grpc.Core;
 using Grpc.Net.Client;
@@ -18,7 +20,8 @@ namespace Chilite.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
             builder.Services.AddSingleton(services =>
             {
@@ -29,11 +32,26 @@ namespace Chilite.Web
                     {
                         HttpHandler = new GrpcWebHandler(new HttpClientHandler())
                     });
-                return new ChatRoom.ChatRoomClient(chanel); 
+                return new ChatRoom.ChatRoomClient(chanel);
             });
 
+            builder.Services.AddBlazoredLocalStorage();
 
-            await builder.Build().RunAsync();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            var defaultCultrure = new CultureInfo("ru-Ru");
+
+            var host = builder.Build();
+
+            var culture = await host.Services.GetService<ILocalStorageService>().GetItemAsStringAsync("lang_culture");
+
+            if (culture != null) 
+                defaultCultrure = new CultureInfo(culture);
+
+            CultureInfo.CurrentCulture = defaultCultrure;
+            CultureInfo.CurrentUICulture = defaultCultrure;
+
+            await host.RunAsync();
         }
     }
 }
